0cda83d7eaea7f4fe4572f4cd10fd7b08717e456
Tiana Govender <104756284+TianaGovender@users.noreply.github.com>
2022-10-11 20:59:05 +0200

    added functions for task 3 and 4

--- sort.h
+++ sort.h
@@ -24,5 +24,7 @@ void bubble_sort(int *array, size_t size);
 void swap_i(int *i, int *j);
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
