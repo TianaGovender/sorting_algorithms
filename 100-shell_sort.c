b90866eef5c6f604115405579366cd8bb86d26eb
Tiana Govender <104756284+TianaGovender@users.noreply.github.com>
2022-10-11 21:27:10 +0200

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,17 +1,17 @@
 #include "sort.h"
 
 /**
- * swap_i - Swap two integers in an array.
- * @i: The first integer to swap.
- * @j: The second integer to swap.
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
  */
-void swap_i(int *i, int *j)
+void swap_ints(int *a, int *b)
 {
 	int tmp;
 
-	tmp = *i;
-	*i = *j;
-	*j = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -24,23 +24,23 @@ void swap_i(int *i, int *j)
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t g, i, j;
+	size_t gap, i, j;
 
 	if (array == NULL || size < 2)
 		return;
 
-	for (g = 1; g < (size / 3);)
-		g = g * 3 + 1;
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
 
-	for (; g >= 1; g /= 3)
+	for (; gap >= 1; gap /= 3)
 	{
-		for (i = g; i < size; i++)
+		for (i = gap; i < size; i++)
 		{
 			j = i;
-			while (j >= g && array[j - g] > array[j])
+			while (j >= gap && array[j - gap] > array[j])
 			{
-				swap_i(array + j, array + (j - g));
-				j -= g;
+				swap_ints(array + j, array + (j - gap));
+				j -= gap;
 			}
 		}
 		print_array(array, size);
