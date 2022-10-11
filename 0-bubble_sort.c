9e9024ddcdb49a1289207b838ee63e395b309180
Tiana Govender <104756284+TianaGovender@users.noreply.github.com>
2022-10-11 20:43:59 +0200

    checking format

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,44 +2,44 @@
 #include <stdbool.h>
 
 /**
- * i_swap - swaps 2 integers in an array
- * @i: First integer
- * @j: second integer
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
  */
-
-void i_swap(int *i, int *j)
+void swap_ints(int *i, int *j)
 {
-	int temp;
+	int tmp;
 
-	temp = *i;
+	tmp = *i;
 	*i = *j;
-	*j = temp;
+	*j = tmp;
 }
 
 /**
- * bubble_sort - sort an array in ascending order
- * @array: array of integers
- * @size: teh size of the array
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
  */
-
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, len = size;
-	bool b = false;
+	bool bubbly = false;
 
 	if (array == NULL || size < 2)
 		return;
 
-	while (b == false)
+	while (bubbly == false)
 	{
-		b = true;
+		bubbly = true;
 		for (i = 0; i < len - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
-				i_swap(array + i, array + i + 1);
+				swap_ints(array + i, array + i + 1);
 				print_array(array, size);
-				b = false;
+				bubbly = false;
 			}
 		}
 		len--;
