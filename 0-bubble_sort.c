c4f4fee8245b8b8d0e48df193274a9df8b2e2f9b
Tiana Govender <104756284+TianaGovender@users.noreply.github.com>
2022-10-15 15:36:37 +0200

    Edited function to check

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,47 +1,35 @@
 #include "sort.h"
-#include <stdbool.h>
+#include <stdio.h>
 
 /**
- * swap_i - Swap two integers in an array.
- * @i: The first integer.
- * @j: The second integer.
- */
-void swap_i(int *i, int *j)
-{
-	int tmp;
-
-	tmp = *i;
-	*i = *j;
-	*j = tmp;
-}
-
-/**
- * bubble_sort - Sort an array of integers in ascending order.
- * @array: An array of integers.
- * @size: The size of the array.
+ * bubble_sort - sorts an array of integers in ascending order
  *
- * Description: Prints the array after each swap.
+ * @array: The array of integer
+ * @size: The size of the array
+ *
+ * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, len = size;
-	bool bubbly = false;
+	size_t i;
+	int temp, s;
 
-	if (array == NULL || size < 2)
+	if (!array || size == 0)
 		return;
 
-	while (bubbly == false)
-	{
-		bubbly = true;
-		for (i = 0; i < len - 1; i++)
+	do {
+		s = 1;
+		for (i = 0; i < size - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
-				swap_i(array + i, array + i + 1);
+				s = 0;
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
 				print_array(array, size);
-				bubbly = false;
 			}
 		}
-		len--;
-	}
+
+	} while (s == 0);
 }
